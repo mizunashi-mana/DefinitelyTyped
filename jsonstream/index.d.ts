512640a9a0c67f657b018441ff7f659b1bd91e94
Mizunashi Mana <mizunashi_mana@mma.club.uec.ac.jp>
2016-12-28 17:21:30 +0900

    Fix type definition

--- jsonstream/index.d.ts
+++ jsonstream/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for JSONStream v0.8.0
+// Type definitions for JSONStream 0.8
 // Project: http://github.com/dominictarr/JSONStream
 // Definitions by: Bart van der Schoor <https://github.com/Bartvds>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -11,8 +11,7 @@ export interface Options {
     recurse: boolean;
 }
 
-export declare function parse(pattern: any): NodeJS.ReadWriteStream;
-export declare function parse(patterns: any[]): NodeJS.ReadWriteStream;
+export declare function parse(pattern?: any | any[]): NodeJS.ReadWriteStream;
 
 export declare function stringify(): NodeJS.ReadWriteStream;
 export declare function stringify(open: string, sep: string, close: string): NodeJS.ReadWriteStream;
--- jsonstream/jsonstream-tests.ts
+++ jsonstream/jsonstream-tests.ts
@@ -1,14 +1,44 @@
+import JSONStream = require('jsonstream');
 
-import json = require('jsonstream');
+var transform: NodeJS.ReadWriteStream;
 
-var read: NodeJS.ReadableStream;
-var write: NodeJS.WritableStream;
+transform = JSONStream.parse();
+transform = JSONStream.parse('*');
+transform = JSONStream.parse(['foo/*', 'bar/*']);
 
-read = read.pipe(json.parse('*'));
-read = read.pipe(json.parse(['foo/*', 'bar/*']));
+transform = JSONStream.stringify();
+transform = JSONStream.stringify('{', ',', '}');
 
-read = json.stringify();
-read = json.stringify('{', ',', '}');
+transform = JSONStream.stringifyObject();
+transform = JSONStream.stringifyObject('{', ',', '}');
 
-read = json.stringifyObject();
-read = json.stringifyObject('{', ',', '}');
+// from https://www.npmjs.com/package/JSONStream#examples
+{
+    var stream = JSONStream.parse(['rows', true, 'doc']); //rows, ANYTHING, doc
+
+    stream.on('data', function(data: any) {
+        console.log('received:', data);
+    });
+    //emits anything from _before_ the first match
+    stream.on('header', function (data: any) {
+        console.log('header:', data); // => {"total_rows":129,"offset":0}
+    });
+}
+
+{
+    var stream = JSONStream.parse(['rows', true, 'doc', {emitKey: true}]); //rows, ANYTHING, doc, items in docs with keys
+
+    stream.on('data', function(data: any) {
+        console.log('key:', data.key);
+        console.log('value:', data.value);
+    });
+}
+
+{
+    var stream = JSONStream.parse(['rows', true, 'doc', {emitPath: true}]); //rows, ANYTHING, doc, items in docs with keys
+
+    stream.on('data', function(data: any) {
+        console.log('path:', data.path);
+        console.log('value:', data.value);
+    });
+}
